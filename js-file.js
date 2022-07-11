ecf55d4873c24f38b9a15864d476669596d70844
NateTewolde <tewolde.nate@gmail.com>
2022-07-10 20:00:58 -0700

    Create module for holding and returning the gameboard choices

---
+++ js-file.js
@@ -0,0 +1,8 @@
+let gameBoardMaker = (function (player) {
+  let gameBoardArray = ["X", "O", "X", "O", "X", "O", "X"];
+  return {
+    gameBoard: gameBoardArray,
+  };
+})();
+
+let displayController = (function (gameBoard) {})();
