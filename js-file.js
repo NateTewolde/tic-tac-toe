408997c2a50704f48bb348b8f3fbb9ff1abdf83b
NateTewolde <tewolde.nate@gmail.com>
2022-07-13 04:48:13 -0700

    Board now correctly populates, switching between players

--- js-file.js
+++ js-file.js
@@ -1,8 +1,48 @@
+const Player = (name) => {
+  const choices = [];
+  const getName = () => name;
+  const makeMark = (choice) => {
+    choices.push(choice);
+    gameBoardMaker.gameBoard(name, choice);
+    displayController.displayBoard();
+  };
+  getChoices = () => choices;
+  return { getChoices, getName, makeMark };
+};
+
+const playGame = (function () {
+  const playerOne = Player("playerOne");
+  const playerTwo = Player("playerTwo");
+  players = [playerOne];
+
+  function getCurrentPlayer() {
+    let currentPlayer = players[players.length - 1];
+    return currentPlayer;
+  }
+
+  function updateCurrentPlayer() {
+    let currentPlayer = getCurrentPlayer();
+    if (currentPlayer == playerOne) {
+      players.push(playerTwo);
+    }
+    if (currentPlayer == playerTwo) {
+      players.push(playerOne);
+    }
+  }
+  return {
+    currentPlayer: getCurrentPlayer,
+    updateCurrentPlayer: updateCurrentPlayer,
+  };
+})();
+
 const gameBoardMaker = (function () {
   let gameBoardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
   function gameBoard(player, choice) {
     let mark = gameBoardArray[choice];
+    if (mark == "X" || mark == "O") {
+      return -1;
+    }
     if (player == "playerOne") {
       mark = "X";
     }
@@ -46,37 +86,38 @@ const displayController = (function () {
     let gameboardTemp = gameBoardMaker.gameBoard();
     let displayTemp = makeBoard(gameboardTemp);
     container.appendChild(displayTemp);
+    formatCells();
   }
+  function formatCells() {
+    const cells = document.querySelectorAll(".cell");
+    console.log(cells.length);
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        let gameBoardIndex = cell.getAttribute("data-ID");
+        let currentPlayer = playGame.currentPlayer();
+        playGame.updateCurrentPlayer();
+        console.log(currentPlayer.getName());
+        currentPlayer.makeMark(gameBoardIndex);
+      });
+    });
+  }
+
   displayBoard();
   return {
     displayBoard: displayBoard,
   };
 })();
 
-//make factory function for players
-const Player = (name) => {
-  return { name };
-};
-
-const formatCells = (function () {
-  const cells = document.querySelectorAll(".cell");
-  console.log(cells.length);
-  cells.forEach((cell) => {
+const formatElements = (function () {
+  function formatStart() {
+    const cell = document.querySelector(".start-btn");
     cell.addEventListener("click", () => {
-      let gameBoardIndex = cell.getAttribute("data-ID");
-      console.log(gameBoardIndex);
+      console.log("pp");
     });
-  });
+  }
 })();
 
-const formatStart = (function () {
-  const cell = document.querySelector(".start-btn");
-  cell.addEventListener("click", () => {
-    console.log("pp");
-  });
-})();
-
-const playGame = (function () {})();
+//make factory function for players
 
 //helper function
 function removeAllChildNodes(parent) {
@@ -92,6 +133,3 @@ function checkForBoard() {
   }
   return true;
 }
-
-gameBoardMaker.gameBoard("playerOne", 4);
-displayController.displayBoard();
